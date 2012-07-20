490aa18448013ffbc11ac3250b296c01d3748a7e
Dave Sayles <sayles.dave@gmail.com>
2012-07-20 09:46:30 -0400

    Added get_ports_by_vid_pid to list_ports_osx, unit tests

--- serial/tools/list_ports_osx.py
+++ serial/tools/list_ports_osx.py
@@ -9,6 +9,7 @@
 
 import ctypes
 from ctypes import util
+import re
 
 iokit = ctypes.cdll.LoadLibrary(ctypes.util.find_library('IOKit'))
 cf = ctypes.cdll.LoadLibrary(ctypes.util.find_library('CoreFoundation'))
@@ -157,6 +158,41 @@ def GetIOServicesByType(service_type):
 
     return services
 
+def get_info_from_serial_identifier(identifier_string):
+    data = {}
+    try:
+        vid, pid, serial_number = re.search('VID:PID=([0-9A-Fa-f]*):([0-9A-Fa-f]*) SNR=(\w*)', identifier_string).groups()
+        data['VID'] = int(vid, 16)
+        data['PID'] = int(pid, 16)
+        data['iSerial'] = serial_number
+    except AttributeError:
+        pass
+    return data   
+
+def check_if_port_is_replicator(vid, pid, data):
+    val = False
+    try:
+        if data['VID'] == vid and data['PID'] == pid:
+            val = True
+    except KeyError:
+        pass
+    return val
+
+def get_ports_by_vid_pid(vid, pid):
+    """Given a VID and PID value, return ports 
+    that have those values.
+
+    @param str vid: The VID value for a port
+    @param str pid: The PID value for a port
+    @return iterator: Ports that are currently active with these VID/PID values
+    """
+    ports = comports()
+    for port in ports:
+        data = get_info_from_serial_identifier(port[-1])
+        if check_if_port_is_replicator(vid, pid, data):
+            data['PORT'] = port[0]
+            yield data
+
 def comports():
     # Scan for all iokit serial ports
     services = GetIOServicesByType('IOSerialBSDClient')
@@ -189,7 +225,8 @@ def comports():
 
 # test
 if __name__ == '__main__':
-    import serial
-
-    for port, desc, hwid in sorted(comports()):
-        print "%s: %s [%s]" % (port, desc, hwid)
+    #import serial
+    for port in get_ports_by_vid_pid(int('23c1', 16), int('d314', 16)):
+        print port
+    #for port, desc, hwid in sorted(comports()):
+    #    print "%s: %s [%s]" % (port, desc, hwid)
---
+++ test/test_list_ports_osx.py
@@ -0,0 +1,65 @@
+import unittest
+import serial.tools.list_ports_osx
+
+class TestListPortsOsx(unittest.TestCase):
+
+    def test_blank_string(self):
+        self.assertEquals(serial.tools.list_ports_osx.get_info_from_serial_identifier(''), {})
+
+    def test_not_usb_device(self):
+        self.assertEquals(serial.tools.list_ports_osx.get_info_from_serial_identifier('abcdefg'), {})
+
+    def test_good_params_upper_case(self):
+        identifier_string = 'USB VID:PID=12AB:34CD SNR=56Ef'
+        expected_info = {
+          'VID' : 4779,
+          'PID' : 13517,
+          'iSerial' : '56Ef'
+          }
+
+        self.assertEquals(
+            serial.tools.list_ports_osx.get_info_from_serial_identifier(identifier_string),
+            expected_info
+          )
+
+    def test_check_if_port_is_replicator_empty_data(self):
+        data = {}
+        val = serial.tools.list_ports_osx.check_if_port_is_replicator(4779, 13517, data)
+        self.assertFalse(val)
+
+    def test_check_if_port_is_replicator_mismatched_vid(self):
+        vid = 4779
+        pid = 13517
+        data = {
+            'VID'   :   vid+1,
+            'PID'   :   pid,
+            'iSerial'   :   '56ef'
+            }
+        val = serial.tools.list_ports_osx.check_if_port_is_replicator(vid, pid, data)
+        self.assertFalse(val)
+
+    def test_check_if_port_is_replicator_mismatched_pid(self):
+        vid = 4779
+        pid = 13517
+        data = {
+            'VID'   :   vid,
+            'PID'   :   pid+1,
+            'iSerial'   :   '56ef',
+            }
+        val = serial.tools.list_ports_osx.check_if_port_is_replicator(vid, pid, data)
+        self.assertFalse(val)
+
+    def test_check_if_port_is_replicator_good_values(self):
+        vid = 4779
+        pid = 13517
+        data = {
+            'VID'   :   vid,
+            'PID'   :   pid,
+            'iSerial'   :   '56ef',
+            }
+        val = serial.tools.list_ports_osx.check_if_port_is_replicator(vid, pid, data)
+        self.assertTrue(val)
+
+
+if __name__ == '__main__':
+    unittest.main()
