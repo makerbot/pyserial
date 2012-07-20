ec698cb27c80393a4bbcfad754a3617345c33781
wdcrith <wdc.rith@gmail.com>
2012-07-20 11:09:28 -0400

    Added find_port_by_vid_pid for posix.  Dave Sayles on the only Linux Machine I could Find

--- serial/tools/list_ports_posix.py
+++ serial/tools/list_ports_posix.py
@@ -191,7 +191,37 @@ this module running...
 """ % (sys.platform, os.name, serial.VERSION))
     raise ImportError("Sorry: no implementation for your platform ('%s') available" % (os.name,))
 
+def get_info_from_serial_identifier(identifier_string):
+    data = {}
+    try:
+        vid, pid, serial_number = re.search('VID:PID=([0-9A-Fa-f]*):([0-9A-Fa-f]*) SNR=(\w*)', identifier_string).groups()
+        data['VID'] = int(vid, 16)
+        data['PID'] = int(pid, 16)
+        data['iSerial'] = serial_number
+    except AttributeError:
+        pass
+    return data   
+
+def check_if_port_is_replicator(vid, pid, data):
+    val = False
+    try:
+        if data['VID'] == vid and data['PID'] == pid:
+            val = True
+    except KeyError:
+        pass
+    return val
+
+def get_ports_by_vid_pid(vid, pid):
+    ports = comports()
+    for port in ports:
+        data = get_info_from_serial_identifier(port[-1])
+        if check_if_port_is_replicator(vid, pid, data):
+            data['PORT'] = port[0]
+            yield data
+
 # test
 if __name__ == '__main__':
-    for port, desc, hwid in sorted(comports()):
-        print "%s: %s [%s]" % (port, desc, hwid)
+    for port in get_ports_by_vid_pid(int('23c1', 16), int('d314', 16)):
+        print port
+#    for port, desc, hwid in sorted(comports()):
+#        print "%s: %s [%s]" % (port, desc, hwid)
