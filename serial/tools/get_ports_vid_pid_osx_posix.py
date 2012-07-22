da4b8109c89028c4e7169ad65a4bd123b0027706
Far McKon <FarMcKon@gmail.com>
2012-07-21 21:16:16 -0400

    simplified and updated get_ports_vid_pid tools

--- serial/tools/get_ports_vid_pid_osx_posix.py
+++ serial/tools/get_ports_vid_pid_osx_posix.py
@@ -1,43 +1,33 @@
 import re
 
-def get_info_from_serial_identifier(identifier_string):
+""" 
+Contains tools for taking a serial object from the standard serial module,
+and intellegently parse out and use PID/VID and iSerial values from it
+"""   
+
+def portdict_from_port(port):
     """
-    Given an identifier_string, parses out the VID, PID and iSerial
+    Given a port object from serial.comport() create a vid/pid/iSerial/port dict if possible
+
     @param str identifier_string: String retrieved from a serial port
-    @return dict: A dictionary of the data we parsed out
+    @return dict: A dictionary of the data we parsed out. On parse error the dict only contains the 'port' value
     """
-    data = {}
+    identifier_string = port[-1]
+    data = {'Port':port}
     try:
         vid, pid, serial_number = re.search('VID:PID=([0-9A-Fa-f]*):([0-9A-Fa-f]*) SNR=(\w*)', identifier_string).groups()
-        data['VID'] = int(vid, 16)
-        data['PID'] = int(pid, 16)
+        data['VID'] = vid 
+        data['PID'] = pid
         data['iSerial'] = serial_number
     except AttributeError:
         pass
     return data   
 
-def check_if_port_is_replicator(vid, pid, data):
-    """
-    Given a VID, PID and data dictionary, determines if this 
-    port is a Replicator.  Due to the way information is retrieved
-    this dict could be empty.
-
-    @param int vid: The Vendor ID
-    @param int pid: The Product ID
-    @param data: A dict of information parsed out of a serial port
-    @return bool: True if this is a replicator, false otherwise
-    """
-    val = False
-    try:
-        if data['VID'] == vid and data['PID'] == pid:
-            val = True
-    except KeyError:
-        pass
-    return val
 
 def get_ports_by_vid_pid(vid, pid):
-    """Given a VID and PID value, return ports 
-    that have those values.
+    """ Given a VID and PID value, scans for available port, and
+	if matches are found, returns a dict of 'VID/PID/iSerial/Port'
+	that have those values.
 
     @param str vid: The VID value for a port
     @param str pid: The PID value for a port
@@ -47,7 +37,9 @@ def get_ports_by_vid_pid(vid, pid):
     ports = comports()
     for port in ports:
         #Parse some info out of the identifier string
-        data = get_info_from_serial_identifier(port[-1])
-        if check_if_port_is_replicator(vid, pid, data):
-            data['PORT'] = port[0]
-            yield data
+        data = portdict_from_port(port[-1])
+        try: 
+            if data['VID'] == vid and data['PID'] == pid:
+            	yield data
+	except:
+		pass
--- test/test_get_ports_vid_pid_osx_posix.py
+++ test/test_get_ports_vid_pid_osx_posix.py
@@ -1,64 +1,39 @@
 import unittest
-import serial.tools.get_ports_vid_pid_osx_posix
+
+import sys
+
+#assuming this is run from ../, this imports the base directory at start of python path,
+#so the ../serial folder is imported as 'import serial' rather than the system serial
+
+sys.path.insert(0,'.') 
+import serial.tools.get_ports_vid_pid_osx_posix as gp_vidpid
 
 class TestListPortsOsx(unittest.TestCase):
 
     def test_blank_string(self):
-        self.assertEquals(serial.tools.get_ports_vid_pid_osx_posix.get_info_from_serial_identifier(''), {})
+        dummyport = ('','')
+        self.assertEquals(
+		gp_vidpid.portdict_from_port(dummyport)
+		,{'Port':dummyport} )
 
     def test_not_usb_device(self):
-        self.assertEquals(serial.tools.get_ports_vid_pid_osx_posix.get_info_from_serial_identifier('abcdefg'), {})
+	dummyport=('','abcdefg')
+        self.assertEquals(gp_vidpid.portdict_from_port(dummyport)
+			  ,{'Port':dummyport})
 
     def test_good_params_upper_case(self):
-        identifier_string = 'USB VID:PID=12AB:34CD SNR=56Ef'
+        dummyport = ('', 'USB VID:PID=12AB:34CD SNR=56Ef')
         expected_info = {
-          'VID' : 4779,
-          'PID' : 13517,
-          'iSerial' : '56Ef'
+          'VID' : '12AB'
+          ,'PID' : '34CD'
+          ,'iSerial' : '56Ef'
+          ,'Port' : dummyport
           }
 
         self.assertEquals(
-            serial.tools.get_ports_vid_pid_osx_posix.get_info_from_serial_identifier(identifier_string),
+            gp_vidpid.portdict_from_port(dummyport),
             expected_info
           )
-
-    def test_check_if_port_is_replicator_empty_data(self):
-        data = {}
-        val = serial.tools.get_ports_vid_pid_osx_posix.check_if_port_is_replicator(4779, 13517, data)
-        self.assertFalse(val)
-
-    def test_check_if_port_is_replicator_mismatched_vid(self):
-        vid = 4779
-        pid = 13517
-        data = {
-            'VID'   :   vid+1,
-            'PID'   :   pid,
-            'iSerial'   :   '56ef'
-            }
-        val = serial.tools.get_ports_vid_pid_osx_posix.check_if_port_is_replicator(vid, pid, data)
-        self.assertFalse(val)
-
-    def test_check_if_port_is_replicator_mismatched_pid(self):
-        vid = 4779
-        pid = 13517
-        data = {
-            'VID'   :   vid,
-            'PID'   :   pid+1,
-            'iSerial'   :   '56ef',
-            }
-        val = serial.tools.get_ports_vid_pid_osx_posix.check_if_port_is_replicator(vid, pid, data)
-        self.assertFalse(val)
-
-    def test_check_if_port_is_replicator_good_values(self):
-        vid = 4779
-        pid = 13517
-        data = {
-            'VID'   :   vid,
-            'PID'   :   pid,
-            'iSerial'   :   '56ef',
-            }
-        val = serial.tools.get_ports_vid_pid_osx_posix.check_if_port_is_replicator(vid, pid, data)
-        self.assertTrue(val)
 
 
 if __name__ == '__main__':
